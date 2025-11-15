dea8b8fa0724a14b7f3301a0e4aa04ad1bdd58a1
Kolya Russkikh <kolya.russkikh@rejuve.ai>
2025-11-15 19:48:46 +0100

    separating error messages for incorrect payload and runtime error

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ def predict_endpoint():
         response = {
             "code": status_code,
             "biological_age": None,
-            "message": "Prediction failed",
+            "message": "message": "Invalid payload" if status_code == 400 else "Prediction failed",
             "errors": result['errors'],
             "warnings": result['warnings']
         }
